773562471038d99dacec114861730ce4d4c3277d
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-21 16:53:15 +0200

    webconfig for swagger fix

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
@@ -13,17 +13,17 @@ public class WebConfig implements WebMvcConfigurer {
               .allowedHeaders("*");
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
-        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
+//        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+//        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+//        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/swagger-ui.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
