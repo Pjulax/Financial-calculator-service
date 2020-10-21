f156a1a6aeddecbbd7ffddfc6a8a6e18f70cdb6f
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-21 20:33:28 +0200

    undo some changes to only extended webMvcConfigurer config

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
@@ -1,10 +1,8 @@
 package pl.fintech.metisfinancialcalculator.fincalcservice;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
-/*
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -29,33 +27,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
-*/
 
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
-            }
-            @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
-                registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-                registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-                registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
-            }
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("/api/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-                registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-            }
-        };
-    }
-}
--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
@@ -32,8 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
 
             // Disable CSRF (cross site request forgery)
-            http.cors(withDefaults())
-                    .csrf().disable();
+            http.csrf().disable();
 
             // No session will be created or used by spring security
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -63,8 +62,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
 
             // Disable CSRF (cross site request forgery)
-            http.cors(withDefaults())
-                    .csrf().disable();
+            http.csrf().disable();
 
             // No session will be created or used by spring security
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
