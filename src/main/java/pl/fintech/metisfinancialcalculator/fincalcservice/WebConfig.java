0a3c4f4f91a7647ff30e7b4a5d40d9beb1f3f0c3
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-21 20:02:42 +0200

    antMatchers check with /api prefix, unlocked cors, changed and used in websecurityconfig

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/WebConfig.java
@@ -1,29 +1,61 @@
-//package pl.fintech.metisfinancialcalculator.fincalcservice;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.*;
-//
-//@Configuration
-////@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//      registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-//              .allowedHeaders("*");
-//    }
-//
-////    @Override
-////    public void addViewControllers(ViewControllerRegistry registry) {
-////        registry.addRedirectViewController("/v2/api-docs", "/v2/api-docs");
-////        registry.addRedirectViewController("/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
-////        registry.addRedirectViewController("/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
-////        registry.addRedirectViewController("/swagger-resources", "/swagger-resources");
-////    }
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-//}
+package pl.fintech.metisfinancialcalculator.fincalcservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+/*
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+              .allowedHeaders("*");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
+*/
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+                registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+                registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+                registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+            }
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/api/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+                registry.addResourceHandler("/api/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+            }
+        };
+    }
+}
--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
@@ -32,13 +32,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
 
             // Disable CSRF (cross site request forgery)
-            http.csrf().disable();
+            http.cors(withDefaults())
+                    .csrf().disable();
 
             // No session will be created or used by spring security
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
             // Entry points
-            http.antMatcher("/users/signin")
+            http.antMatcher("/api/users/signin")
                 .authorizeRequests(authorize -> authorize
                     .anyRequest().hasAnyRole("USER","ADMIN"));
                     // Disallow everything else..
@@ -62,20 +63,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
 
             // Disable CSRF (cross site request forgery)
-            http.csrf().disable();
+            http.cors(withDefaults())
+                    .csrf().disable();
 
             // No session will be created or used by spring security
             http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
             // Entry points
             http.authorizeRequests()//
-                    .antMatchers("/v2/api-docs").permitAll()
-                    .antMatchers("/swagger-resources").permitAll()
-                    .antMatchers("/swagger-resources/configuration/ui").permitAll()
-                    .antMatchers("/swagger-resources/configuration/security").permitAll()
-                    .antMatchers("/users/signin").permitAll()//
-                    .antMatchers("/users/signup").permitAll()//
-                    .antMatchers("/h2-console/**/**").permitAll()
+                    .antMatchers("/api/v2/api-docs").permitAll()
+                    .antMatchers("/api/swagger-resources").permitAll()
+                    .antMatchers("/api/swagger-resources/configuration/ui").permitAll()
+                    .antMatchers("/api/swagger-resources/configuration/security").permitAll()
+                    .antMatchers("/api/users/signin").permitAll()//
+                    .antMatchers("/api/users/signup").permitAll()//
+                    .antMatchers("/api/h2-console/**/**").permitAll()
                     // Disallow everything else..
                     .anyRequest().authenticated();
 
@@ -93,17 +95,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         // Allow swagger to be accessed without authentication
-        web.ignoring().antMatchers("/v2/api-docs")//
-                .antMatchers("/swagger-resources/**")//
-                .antMatchers("/swagger-ui.html")//
-                .antMatchers("/configuration/**")//
-                .antMatchers("/webjars/**")//
-                .antMatchers("/public")//;
+        web.ignoring().antMatchers("/api/v2/api-docs")//
+                .antMatchers("/api/swagger-resources/**")//
+                .antMatchers("/api/swagger-ui.html")//
+                .antMatchers("/api/configuration/**")//
+                .antMatchers("/api/webjars/**")//
+                .antMatchers("/api/public")//;
 
                 // Un-secure H2 Database (for testing purposes, H2 console shouldn't be unprotected in production)
                 .and()
                 .ignoring()
-                .antMatchers("/h2-console/**/**");
+                .antMatchers("/api/h2-console/**/**");
     }
 
     @Bean
