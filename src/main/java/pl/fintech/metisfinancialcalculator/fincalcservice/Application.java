896f857d4a8844b74b8a07b9a69ff4ef54a8843a
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-25 19:44:58 +0100

    test without h2 accounts created on start

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/Application.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/Application.java
@@ -16,11 +16,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application implements CommandLineRunner { // implemented interface is to delete after auth done
-
-    // user service is to delete after auth done
-    @Autowired
-    UserService userService;
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -31,21 +27,4 @@ public class Application implements CommandLineRunner { // implemented interface
         return new ModelMapper();
     }
 
-    //to delete after authentication and authorization will be tested to be done
-    @Override
-    public void run(String... params) throws Exception {
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setPassword("admin");
-        admin.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_ADMIN)));
-
-        userService.signup(admin);
-
-        User client = new User();
-        client.setUsername("client");
-        client.setPassword("client");
-        client.setRoles(new ArrayList<Role>(Arrays.asList(Role.ROLE_CLIENT)));
-
-        userService.signup(client);
-    }
 }
