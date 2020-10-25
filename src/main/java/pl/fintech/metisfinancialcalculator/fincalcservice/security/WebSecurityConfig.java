10fba4a6aa7b6af06d752258cd4546471bf41e03
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-25 23:36:22 +0100

    Basic auth changed to post UserData body, JWT auth header now is AuthorizationJwt

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/security/WebSecurityConfig.java
@@ -25,86 +25,134 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Configuration
-    @Order(1)
-    public static class BasicAuthConfigurationAdapter extends WebSecurityConfigurerAdapter {
+// unlock when there will be no middleware auth
+//    @Configuration
+//    @Order(1)
+//    public static class BasicAuthConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//
+//        @Autowired
+//        private  JwtTokenProvider jwtTokenProvider;
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//
+//            // Disable CSRF (cross site request forgery)
+//            http.csrf().disable();
+//
+//            // No session will be created or used by spring security
+//            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//            // Entry points
+//            http.antMatcher("/users/signin")
+//                .authorizeRequests(authorize -> authorize
+//                    .anyRequest().hasAnyRole("USER","ADMIN"));
+//                    // Disallow everything else..
+//
+//            // If a user try to access a resource without having enough permissions
+//            http.exceptionHandling().accessDeniedPage("/login");
+//
+//            // Optional, if you want to test the API from a browser
+//            http.httpBasic(withDefaults());
+//        }
+//    }
+//
+//    @Configuration
+//    @Order(2)
+//    public static class JwtConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//
+//        @Value("${management.server.port}")
+//        private int managementPort;
+//
+//        @Autowired
+//        private  JwtTokenProvider jwtTokenProvider;
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//
+//            // Disable CSRF (cross site request forgery)
+//            http.csrf().disable();
+//
+//            // No session will be created or used by spring security
+//            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//            // Entry points
+//            http.authorizeRequests()//
+//                    .antMatchers("/v2/api-docs").permitAll()
+//                    .antMatchers("/swagger-resources").permitAll()
+//                    .antMatchers("/swagger-resources/configuration/ui").permitAll()
+//                    .antMatchers("/swagger-resources/configuration/security").permitAll()
+//                    .antMatchers("/users/signin").permitAll()//
+//                    .antMatchers("/users/signup").permitAll()//
+//                    .antMatchers("/h2-console/**/**").permitAll()
+//
+//                    .antMatchers("/portfolios").permitAll()
+//                    .antMatchers("/investments").permitAll()
+//                    .antMatchers("/portfolios/**").permitAll()
+//                    .antMatchers("/investments/**").permitAll()
+//                    .requestMatchers(checkPort(managementPort)).permitAll()
+//                    // Disallow everything else..
+//                    .anyRequest().authenticated();
+//
+//            // If a user try to access a resource without having enough permissions
+//            http.exceptionHandling().accessDeniedPage("/login");
+//
+//            // Apply JWT
+//            http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+//
+//            // Optional, if you want to test the API from a browser
+//            // http.httpBasic();
+//        }
+//
+//        private RequestMatcher checkPort(final int port) {
+//            return (HttpServletRequest request) -> port == request.getLocalPort();
+//        }
+//    }
 
-        @Autowired
-        private  JwtTokenProvider jwtTokenProvider;
+    @Value("${management.server.port}")
+    private int managementPort;
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
+    @Autowired
+    private  JwtTokenProvider jwtTokenProvider;
 
-            // Disable CSRF (cross site request forgery)
-            http.csrf().disable();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-            // No session will be created or used by spring security
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Disable CSRF (cross site request forgery)
+        http.csrf().disable();
 
-            // Entry points
-            http.antMatcher("/users/signin")
-                .authorizeRequests(authorize -> authorize
-                    .anyRequest().hasAnyRole("USER","ADMIN"));
-                    // Disallow everything else..
+        // No session will be created or used by spring security
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-            // If a user try to access a resource without having enough permissions
-            http.exceptionHandling().accessDeniedPage("/login");
+        // Entry points
+        http.authorizeRequests()//
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/swagger-resources").permitAll()
+                .antMatchers("/swagger-resources/configuration/ui").permitAll()
+                .antMatchers("/swagger-resources/configuration/security").permitAll()
+                .antMatchers("/users/signin").permitAll()//
+                .antMatchers("/users/signup").permitAll()//
+                .antMatchers("/h2-console/**/**").permitAll()
+                //TODO - change authentication range
+                .antMatchers("/portfolios").permitAll()
+                .antMatchers("/investments").permitAll()
+                .antMatchers("/portfolios/**").permitAll()
+                .antMatchers("/investments/**").permitAll()
+                .requestMatchers(checkPort(managementPort)).permitAll()
+                // Disallow everything else..
+                .anyRequest().authenticated();
 
-            // Optional, if you want to test the API from a browser
-            http.httpBasic(withDefaults());
-        }
+        // If a user try to access a resource without having enough permissions
+        http.exceptionHandling().accessDeniedPage("/login");
+
+        // Apply JWT
+        http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+
+        // Optional, if you want to test the API from a browser
+        // http.httpBasic();
     }
 
-    @Configuration
-    @Order(2)
-    public static class JwtConfigurationAdapter extends WebSecurityConfigurerAdapter {
-
-        @Value("${management.server.port}")
-        private int managementPort;
-
-        @Autowired
-        private  JwtTokenProvider jwtTokenProvider;
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-
-            // Disable CSRF (cross site request forgery)
-            http.csrf().disable();
-
-            // No session will be created or used by spring security
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-            // Entry points
-            http.authorizeRequests()//
-                    .antMatchers("/v2/api-docs").permitAll()
-                    .antMatchers("/swagger-resources").permitAll()
-                    .antMatchers("/swagger-resources/configuration/ui").permitAll()
-                    .antMatchers("/swagger-resources/configuration/security").permitAll()
-                    .antMatchers("/users/signin").permitAll()//
-                    .antMatchers("/users/signup").permitAll()//
-                    .antMatchers("/h2-console/**/**").permitAll()
-
-                    .antMatchers("/portfolios").permitAll()
-                    .antMatchers("/investments").permitAll()
-                    .antMatchers("/portfolios/**").permitAll()
-                    .antMatchers("/investments/**").permitAll()
-                    .requestMatchers(checkPort(managementPort)).permitAll()
-                    // Disallow everything else..
-                    .anyRequest().authenticated();
-
-            // If a user try to access a resource without having enough permissions
-            http.exceptionHandling().accessDeniedPage("/login");
-
-            // Apply JWT
-            http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
-
-            // Optional, if you want to test the API from a browser
-            // http.httpBasic();
-        }
-
-        private RequestMatcher checkPort(final int port) {
-            return (HttpServletRequest request) -> port == request.getLocalPort();
-        }
+    private RequestMatcher checkPort(final int port) {
+        return (HttpServletRequest request) -> port == request.getLocalPort();
     }
 
     @Override
