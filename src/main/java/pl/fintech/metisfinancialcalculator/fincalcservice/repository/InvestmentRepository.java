15aa30148f1219d49cbfcd2c4cb6d0c60c4de89e
Maciag312 <bartekmaciag@icloud.com>
2020-10-28 00:15:37 +0100

    add service restriction

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/repository/InvestmentRepository.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/repository/InvestmentRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.fintech.metisfinancialcalculator.fincalcservice.model.Investment;
 
+import java.util.List;
+
 @Repository
 public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 }
--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/repository/UserRepository.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/repository/UserRepository.java
@@ -3,9 +3,11 @@ package pl.fintech.metisfinancialcalculator.fincalcservice.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import pl.fintech.metisfinancialcalculator.fincalcservice.model.Portfolio;
 import pl.fintech.metisfinancialcalculator.fincalcservice.model.User;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
@@ -16,5 +18,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Transactional
     void deleteByUsername(String username);
+
+    Optional<User> findUserByPortfoliosContaining(Portfolio portfolio);
 
 }
