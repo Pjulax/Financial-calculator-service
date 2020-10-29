f1a19271546bc009c339f56aed8c7efcbd199107
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-29 20:26:05 +0100

    portfolioController documented

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/controller/PortfolioController.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/controller/PortfolioController.java
@@ -1,9 +1,6 @@
 package pl.fintech.metisfinancialcalculator.fincalcservice.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +21,8 @@ public class PortfolioController {
 
     @GetMapping(path = "/hello")
     @ApiOperation(value = "${PortfolioController.hello}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 403, message = "Access denied")})
     public String hello(){
         return "hello";
     }
@@ -31,6 +30,9 @@ public class PortfolioController {
     @GetMapping(path = "/hello2")
     @PreAuthorize("hasRole('ROLE_CLIENT')")
     @ApiOperation(value = "${PortfolioController.hello2}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message = "Unauthorized access"),
+            @ApiResponse(code = 403, message = "Access denied")})
     public String hello2(){
         return "hello";
     }
@@ -41,7 +43,8 @@ public class PortfolioController {
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Something went wrong"),
             @ApiResponse(code = 401, message = "Unauthorized access"),
-            @ApiResponse(code = 404, message = "The user doesn't exist")})
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized")})
     public List<PortfolioNameDTO> getAllPortfoliosNames(){
         return portfolioService.getAllPortfoliosNames();
     }
@@ -50,9 +53,10 @@ public class PortfolioController {
     @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_CLIENT')")
     @ApiOperation(value = "${PortfolioController.all-investments-details}")
     @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 400, message = "Something went wrong or tried to divide by zero"),
+            @ApiResponse(code = 401, message = "Unauthorized access"),
             @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use")})
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized")})
     public PortfolioDetailsDTO getPortfolioAllInvestmentsDetails(){
         return portfolioService.getPortfolioAllInvestmentsDetails();
     }
@@ -62,9 +66,10 @@ public class PortfolioController {
     @ApiOperation(value = "${PortfolioController.portfolios-get}")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 401, message = "Unauthorized access"),
             @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use")})
-    public PortfolioDetailsDTO getPortfolio(@RequestParam(value = "id") Long portfolioId){
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized")})
+    public PortfolioDetailsDTO getPortfolio(@ApiParam("Portfolio Id") @RequestParam(value = "id") Long portfolioId){
         return portfolioService.getPortfolioDetails(portfolioId);
     }
 
@@ -73,9 +78,11 @@ public class PortfolioController {
     @ApiOperation(value = "${PortfolioController.portfolios-post}")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 401, message = "Unauthorized access"),
             @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use")})
-    public Portfolio createPortfolio(@RequestParam(value = "name") String name){
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized"),
+            @ApiResponse(code = 409, message = "Portfolio already exists or provided name is empty")})
+    public Portfolio createPortfolio(@ApiParam("New portfolio name") @RequestParam(value = "name") String name){
         return portfolioService.createPortfolio(name);
     }
 
@@ -84,9 +91,10 @@ public class PortfolioController {
     @ApiOperation(value = "${PortfolioController.portfolios-put}")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 401, message = "Unauthorized access"),
             @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use")})
-    public Portfolio modifyPortfolio(@RequestParam(value="new-name") String name, @RequestParam(value = "id") Long portfolioId){
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized")})
+    public Portfolio modifyPortfolio(@ApiParam("New name for portfolio") @RequestParam(value="new-name") String name, @ApiParam("Modified portfolio Id") @RequestParam(value = "id") Long portfolioId){
         return portfolioService.modifyPortfolio(portfolioId, name);
     }
 
@@ -95,9 +103,10 @@ public class PortfolioController {
     @ApiOperation(value = "${PortfolioController.portfolios-delete}")
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 401, message = "Unauthorized access"),
             @ApiResponse(code = 403, message = "Access denied"),
-            @ApiResponse(code = 422, message = "Username is already in use")})
-    public void removePortfolio(@RequestParam(value = "id") Long portfolioId){
+            @ApiResponse(code = 404, message = "The resource can't be found or access is unauthorized")})
+    public void removePortfolio(@ApiParam("Removed portfolio Id") @RequestParam(value = "id") Long portfolioId){
         portfolioService.removePortfolio(portfolioId);
     }
 
