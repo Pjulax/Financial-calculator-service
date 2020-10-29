e4696ee3c2c8f18002a2c28d3f7c38bd92e70976
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-29 01:17:07 +0100

    UserDataDTO getters and setters deleted due to lombok

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/dto/UserDataDTO.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/dto/UserDataDTO.java
@@ -1,10 +1,17 @@
 package pl.fintech.metisfinancialcalculator.fincalcservice.dto;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import pl.fintech.metisfinancialcalculator.fincalcservice.model.Role;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDataDTO {
 
     @ApiModelProperty(position = 0)
@@ -13,29 +20,5 @@ public class UserDataDTO {
     private String password;
     @ApiModelProperty(position = 2)
     List<Role> roles;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
 
 }
