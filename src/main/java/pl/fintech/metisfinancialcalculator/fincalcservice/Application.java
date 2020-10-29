71003f3ea31beaaeb82b799573661b0e38af8ffe
Paweł Potuczko <pawelpo99@gmail.com>
2020-10-29 01:00:15 +0100

    Application unused imports deleted

--- src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/Application.java
+++ src/main/java/pl/fintech/metisfinancialcalculator/fincalcservice/Application.java
@@ -1,18 +1,10 @@
 package pl.fintech.metisfinancialcalculator.fincalcservice;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import pl.fintech.metisfinancialcalculator.fincalcservice.model.Role;
-import pl.fintech.metisfinancialcalculator.fincalcservice.model.User;
-import pl.fintech.metisfinancialcalculator.fincalcservice.service.UserService;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories
